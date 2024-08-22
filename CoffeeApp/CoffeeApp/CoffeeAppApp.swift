969f150f71d4e3358ecc57dbf17eb9239f517283
Sarvesh Doshi <sarvesh.doshi@in10media.com>
2024-08-22 16:18:16 +0530

    Initial Commit.

---
+++ CoffeeApp/CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Sarvesh Doshi on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
